7227efcb8118b410403b1029de600d1462b8610d
Manmohan Shaw <manmohan@MS.local>
2024-09-08 11:28:59 +0530

    Updating Security Groups

--- security.tf
+++ security.tf
@@ -33,7 +33,6 @@ resource "aws_security_group_rule" "postgresql-egress-kong" {
   from_port = 0
   to_port   = 0
   protocol  = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
   source_security_group_id = aws_security_group.kong.id
 }
 
